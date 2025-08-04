f0ef0d575e3753f816e84c182bdf6b40325a7548
Noob2108 <116654578+Noob2108@users.noreply.github.com>
2025-08-04 20:39:46 +0930

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+function startGame() {
+  const gameArea = document.getElementById('gameArea');
+  gameArea.innerHTML = '<p>Game started! (This is placeholder logic. Full logic comes next.)</p>';
+}
